17076db394333d2980a0dd1f9e844b743baed2fb
CRIZZBIZZ <86725697+CRIZZBIZZ@users.noreply.github.com>
2025-04-15 13:16:14 +0530

    Update cb_data.py

--- plugins/Extra/rename/cb_data.py
+++ plugins/Extra/rename/cb_data.py
@@ -4,6 +4,7 @@
 
 from plugins.Extra.utils import progress_for_pyrogram, convert, humanbytes
 from pyrogram import Client, filters
+from plugins.Extra.rename.filedetect import refunc
 from pyrogram.types import (  InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
@@ -18,20 +19,10 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 @Client.on_callback_query(filters.regex('cancel'))
 async def cancel(bot,update):
-	try:
-           await update.message.delete()
-	except:
-           return
-
-@Client.on_callback_query(filters.regex('rename'))
-async def rename(bot,update):
-	user_id = update.message.chat.id
-	date = update.message.date
-	await update.message.delete()
-	await update.message.reply_text("__𝙿𝚕𝚎𝚊𝚜𝚎 𝙴𝚗𝚝𝚎𝚛 𝙽𝚎𝚠 𝙵𝚒𝚕𝚎𝙽𝚊𝚖𝚎...__",	
-	reply_to_message_id=update.message.reply_to_message.id,  
-	reply_markup=ForceReply(True))
-	
+    try:
+        await update.message.delete()
+    except:
+        return
 
 @Client.on_callback_query(filters.regex("upload"))
 async def doc(bot, update):
@@ -47,7 +38,7 @@ async def doc(bot, update):
             path = await bot.download_media(
                     message=file,
                     progress=progress_for_pyrogram,
-                    progress_args=("**⚠️ Please wait REENU is processing**", ms, c_time))
+                    progress_args=("**⚠️ Please wait ᴛᴇꜱꜱᴀ is in processing**", ms, c_time))
         except Exception as e:
             await ms.edit(e)
             return 
